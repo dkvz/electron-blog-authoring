2e1c23d89424bbe20909776fe2a1a33dfb38618b
DkVZ <bouc@clanz.be>
2018-08-15 19:27:26 +0200

    Added my own menu

--- README.md
+++ README.md
@@ -8,6 +8,19 @@ I then copied the whole thing into my "assets" directory as is.
 
 Quick link to available components: http://photonkit.com/components/
 
+## Menus
+Default menu roles here: https://electronjs.org/docs/api/menu-item#roles
+
+I pretty much used their default menu from here: https://electronjs.org/docs/api/menu#examples
+But added File and put it in a separate file (which requires a few imports).
+
+## App name
+The app name is currently all over the place.
+
+The recommended way is to add a key called "productName" in package.json which is allowed to contain spaces etc. (unlike "name").
+
+Then you can access that using app.getName();
+
 ## Styling
 There are component-specific styles in base.css.
 
@@ -16,7 +29,7 @@ There are component-specific styles in base.css.
 * I think I need to register a hook in editor-events to check if the article was modified. Could be a boolean and the first on-change is triggering it. Now for the two Editor components I'll need something else (maybe register an event to on-input at first, then unregister it at first occurence).
 
 ## TODO
-- [ ] Implement default menu from there: https://electronjs.org/docs/api/menu#examples
+- [ ] I should change all my uses of the name elsewhere by a call to app.getName().
 - [ ] Add a Save As menu item.
 - [ ] Main.js doesn't have semicolons (copied it from elsewhere) - Can prettier do something for me there?
 - [ ] There is a flash before the main page is rendered, it appears white for a few seconds. I can probably do something in index.jsx to have a starter content and replace it when the Preact stuff is ready.
---
+++ electron/application-menu.js
@@ -0,0 +1,94 @@
+
+const { app, shell } = require('electron');
+
+const menuTemplate = [
+  {
+    label: 'File',
+    submenu: [
+      {role: 'quit'}
+    ]
+  },
+  {
+    label: 'Edit',
+    submenu: [
+      {role: 'undo'},
+      {role: 'redo'},
+      {type: 'separator'},
+      {role: 'cut'},
+      {role: 'copy'},
+      {role: 'paste'},
+      {role: 'pasteandmatchstyle'},
+      {role: 'delete'},
+      {role: 'selectall'}
+    ]
+  },
+  {
+    label: 'View',
+    submenu: [
+      {role: 'reload'},
+      {role: 'forcereload'},
+      {role: 'toggledevtools'},
+      {type: 'separator'},
+      {role: 'resetzoom'},
+      {role: 'zoomin'},
+      {role: 'zoomout'},
+      {type: 'separator'},
+      {role: 'togglefullscreen'}
+    ]
+  },
+  {
+    role: 'window',
+    submenu: [
+      {role: 'minimize'}
+    ]
+  },
+  {
+    role: 'help',
+    submenu: [
+      {
+        label: 'Learn More',
+        click () { shell.openExternal('https://dkvz.eu') }
+      }
+    ]
+  }
+]
+
+if (process.platform === 'darwin') {
+  menuTemplate.unshift({
+    label: app.getName(),
+    submenu: [
+      {role: 'about'},
+      {type: 'separator'},
+      {role: 'services', submenu: []},
+      {type: 'separator'},
+      {role: 'hide'},
+      {role: 'hideothers'},
+      {role: 'unhide'},
+      {type: 'separator'},
+      {role: 'quit'}
+    ]
+  })
+
+  // Edit menu
+  menuTemplate[1].submenu.push(
+    {type: 'separator'},
+    {
+      label: 'Speech',
+      submenu: [
+        {role: 'startspeaking'},
+        {role: 'stopspeaking'}
+      ]
+    }
+  )
+
+  // Window menu
+  menuTemplate[3].submenu = [
+    {role: 'close'},
+    {role: 'minimize'},
+    {role: 'zoom'},
+    {type: 'separator'},
+    {role: 'front'}
+  ]
+}
+
+module.exports = menuTemplate;
