a283e6243d642dc1359733667c18db8c75c2fcd5
dkvz <bouc@clanz.be>
2018-11-07 15:12:45 +0100

    Currently writing a search box

---
+++ components/SearchBox.jsx
@@ -0,0 +1,60 @@
+const { h, Component } = require("preact");
+
+class SearchBox extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      caseSensitive: props.caseSensitive || false
+    };
+    this.search = this.search.bind(this);
+    this.textFieldKeyUp = this.textFieldKeyUp.bind(this);
+  }
+
+  search(forward) {
+    // We need to call a function provided in props.
+    if (this.textField.value !== '') {
+      this.props.onSearch(
+        {
+          detail: {
+            query: this.textField.value,
+            caseSensitive: this.state.caseSensitive,
+            forward
+          }
+        }
+      );
+    }
+  }
+
+  textFieldKeyUp(e) {
+    if (e.key === 'Enter') {
+      this.search(true);
+    }
+  }
+
+  render() {
+    return (
+      <div className="SearchBox"
+        style={{ display: this.props.show ? 'block' : 'none' }}>
+        <input type="text" 
+          ref={(ref) => this.textField = ref} 
+          onKeyUp={this.textFieldKeyUp}
+          />
+        <span className="chkbox-wrapper">
+          <label title="Enable/disable case sensitive search"
+            className={this.state.caseSensitive && 'checked'}
+            onClick={this.setState({caseSensitive: !this.state.caseSensitive})}>
+            Aa
+          </label>
+          <input type="checkbox" checked={this.state.caseSensitive} />
+        </span>
+        <a href="#" onClick={() => this.search(false)}>&larr;</a>
+        <a href="#" onClick={() => this.search(true)}>&rarr;</a>
+        <a href="#" onClick={this.props.onClose}>&times;</a>
+      </div>
+    );
+  }
+
+}
+
+module.exports = SearchBox;
